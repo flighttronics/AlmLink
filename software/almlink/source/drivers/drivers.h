0fca5ec900df1716bb5254c882b2320dde7b8891
Andreas Kingbaeck <andreas@flighttronics.se>
2013-11-16 13:55:49 +0100

    Added more files

---
+++ software/almlink/source/drivers/drivers.h
@@ -0,0 +1,46 @@
+/******************************************************************************
+ drivers.h for Allmogetracker 3
+
+ Allmogetracker receives data from a GPS receiver and transmits it
+ over amature radio using the APRS protocol version 1.0.
+
+ Copyright (C)2012, Andreas Kingback  (andki234@gmail.com)
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
+/**
+ * @file drivers.h
+ * Common include file for all drivers
+ * @brief This section contains the common area for all drivers
+ *
+ * @author Andreas Kingback
+ * @version 0.00
+ */
+
+#ifndef DRIVERS_H_
+#define DRIVERS_H_
+
+//-------------------------------------------------------------------------------------------------
+//--------  E N U M S  ----------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------
+typedef enum
+{
+    FAIL = -1, SUCCESS = 0
+} T_STATUS;
+
+
+#endif /* DRIVERS_H_ */
+
+
